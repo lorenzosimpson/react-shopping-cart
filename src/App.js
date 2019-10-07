f28cb1c3c89e2fc0ef89ac4fcdfc4d23cf472c9f
Lorenzo Simpson <lorenzosimp@gmail.com>
2019-10-07 16:04:56 -0400

    products data coming in from context

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import React, { useState } from 'react';
 import { Route } from 'react-router-dom';
 import data from './data';
 
+// Contexts
+import { ProductContext } from './contexts/ProductContext';
+
+
 // Components
 import Navigation from './components/Navigation';
 import Products from './components/Products';
@@ -21,24 +25,21 @@ function App() {
 
 	return (
 		<div className="App">
-			<Navigation cart={cart} />
+			<ProductContext.Provider value={{ products, addItem }}> 
+				<Navigation cart={cart} />
 
-			{/* Routes */}
-			<Route
-				exact
-				path="/"
-				render={() => (
-					<Products
-						products={products}
-						addItem={addItem}
-					/>
-				)}
-			/>
+				{/* Routes */}
+				<Route
+					exact
+					path="/"
+					component={Products}
+				/>
 
-			<Route
-				path="/cart"
-				render={() => <ShoppingCart cart={cart} />}
-			/>
+				<Route
+					path="/cart"
+					render={() => <ShoppingCart cart={cart} />}
+				/>
+			</ProductContext.Provider>
 		</div>
 	);
 }
